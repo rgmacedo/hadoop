5401f6f15f8cbc905c92dd19ae47c322eb465188
rgmacedo <ricardomacedo13@gmail.com>
2019-06-06 15:41:06 +0100

    Minor typo

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/datatransfer/Sender.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/datatransfer/Sender.java
@@ -54,6 +54,8 @@ import org.apache.hadoop.util.DataChecksum;
 import org.apache.htrace.core.SpanId;
 import org.apache.htrace.core.Tracer;
 
+import org.apache.commons.logging.Log;
+
 import com.google.protobuf.Message;
 
 /** Sender */
